3549b687ca7007405324da2a6f0310eee474ffc2
Alvin <quhao317@163.com>
2021-10-22 02:34:09 +0800

    [builtin] add set demo

--- basic/builtin/collection.go
+++ basic/builtin/collection.go
@@ -40,3 +40,79 @@ type UserValue struct {
 	birthday string
 	address  string
 }
+
+// 定义 Set 集合结构体
+type Set struct {
+	m   map[interface{}]struct{}
+	cap int
+}
+
+// 创建并初始化 Set 集合对象
+//  cap: Set 集合初始容积
+func NewSet(cap int) *Set {
+	s := Set{m: make(map[interface{}]struct{}, cap), cap: cap}
+	return &s
+}
+
+// 向 Set 集合中添加元素
+func (s *Set) Add(values ...interface{}) {
+	for _, v := range values { // 遍历参数，获取参数值（range 返回的第 2 个值，第 1 个值为下标）
+		s.m[v] = struct{}{} // 以 某个参数值 为 key，空结构为 value，设置 map（相当于只给 map 设置了 key）
+	}
+}
+
+// 从 Set 集合中删除指定的元素
+func (s *Set) Remove(values ...interface{}) {
+	for _, v := range values { // 遍历参数，从 map 中删除参数所表示的 key
+		delete(s.m, v)
+	}
+}
+
+// 判断 元素 是否在 Set 集合中存在
+func (s *Set) Contains(values ...interface{}) bool {
+	for _, v := range values { // 遍历参数，从 map 中查找 参数所表示的 key 是否存在
+		_, ok := s.m[v]
+		if !ok {
+			return false
+		}
+	}
+	return true
+}
+
+// 获取 Set 集合元素个数
+func (s *Set) Len() int {
+	return len(s.m)
+}
+
+// 情况 Set 集合中的元素
+func (s *Set) Clear(i, j int) {
+	s.m = make(map[interface{}]struct{}, s.cap)
+}
+
+// 判断两个 Set 集合是否相同（包含相同的元素)
+func (s *Set) Equal(other *Set) bool {
+	if s.Len() != other.Len() { // 两个集合元素个数是否相同
+		return false // 元素个数不同不能相等
+	}
+
+	for v := range s.m { // 遍历 map，判断 参数 所表示的 key 是否存在
+		if !other.Contains(v) {
+			return false // 某个参数的 key 不存在，则返回 false
+		}
+	}
+	return true
+}
+
+// 判断当前 Set 集合是否另一个集合的 子集
+func (s *Set) IsSubset(other *Set) bool {
+	if s.Len() > other.Len() {
+		return false // 当前集合元素数必须不能大于另一个集合，否则不能成为 子集
+	}
+
+	for v := range s.m {
+		if !other.Contains(v) { // 另一个集合是否包含 当前集合的 所有元素
+			return false
+		}
+	}
+	return true
+}
--- basic/builtin/collection_test.go
+++ basic/builtin/collection_test.go
@@ -409,3 +409,48 @@ func TestList(t *testing.T) {
 	lst.Init()
 	assert.Equal(t, 0, lst.Len())
 }
+
+// 测试 Set 集合
+func TestSet(t *testing.T) {
+	// 初始化并添加元素
+	s1 := NewSet(100)            // 初始化
+	s1.Add(1, 2, 3, 4, 2)        // 批量添加元素
+	assert.Equal(t, 4, s1.Len()) // 实际添加了 4 个元素，重复的 2 只存在 1 份
+
+	// 判断集合是否包含指定值
+	ok := s1.Contains(1)
+	assert.True(t, ok)
+
+	ok = s1.Contains(2, 3, 4) // 多值判断
+	assert.True(t, ok)
+
+	ok = s1.Contains(3, 4, 5) // 5 不在集合中，返回 false
+	assert.False(t, ok)
+
+	// 集合相等判断
+	s2 := NewSet(10) // 产生一个元素相同的集合
+	s2.Add(1, 2, 3, 4)
+
+	ok = s2.Equal(s1) // 两个集合元素是否相同
+	assert.True(t, ok)
+
+	s2.Add("Hello")   // 在其中一个集合中添加新元素
+	ok = s2.Equal(s1) // 此时两个集合不再相同
+	assert.False(t, ok)
+
+	s2.Remove("Hello") // 移除之前添加的新元素
+	ok = s2.Equal(s1)  // 此时两个集合恢复相同
+	assert.True(t, ok)
+
+	// 判断是否为子集
+	ok = s2.IsSubset(s1) // 判断两个相同的集合是否互为子集
+	assert.True(t, ok)
+	ok = s1.IsSubset(s2)
+	assert.True(t, ok)
+
+	s1.Remove(2) // 删除集合元素，此时两个集合不再互为子集
+	ok = s2.IsSubset(s1)
+	assert.False(t, ok)
+	ok = s1.IsSubset(s2)
+	assert.True(t, ok)
+}
