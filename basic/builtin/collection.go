0cecd01a67a53ee7769bc848a35012eca4f5bc8c
Alvin <quhao317@163.com>
2021-10-22 01:53:50 +0800

    [builtin] add map and list demo

--- basic/builtin/collection.go
+++ basic/builtin/collection.go
@@ -5,64 +5,38 @@ package builtin
 
 import (
 	"container/list"
-	"fmt"
 )
 
-// 定义 IntArray 类型，为整数数组的别名
-type IntArray []int
+// 将列表转化为切片
+func ToSlice(l *list.List) []interface{} {
+	// 生成一个 cap 为列表长度的空切片
+	slice := make([]interface{}, 0, l.Len())
 
-// 创建一个新 整型 数组
-// 	len: 数组的初始长度
-//	cap: 数组的最大长度
-func NewInts(len int, cap int) IntArray {
-	return make(IntArray, len, cap) // make([]int, len, cap)
-}
-
-func (i *IntArray) Append(n int) {
-	*i = append(*i, n)
-}
-
-func (i *IntArray) Remove(n int) {
-	*i = append((*i)[:n], (*i)[n+1:]...)
-}
-
-func (i *IntArray) Clear() {
-	*i = make(IntArray, 0)
-}
-
-func (i IntArray) Size() int {
-	return len(i)
-}
-
-type Any interface{}
-
-func ListAssign(items ...Any) *list.List {
-	lst := list.New()
-	for _, v := range items {
-		lst.PushBack(v)
+	// 遍历列表，将列表元素依次加入切片中
+	for iter := l.Front(); iter != nil; iter = iter.Next() {
+		slice = append(slice, iter.Value)
 	}
-	return lst
+	return slice
 }
 
-func ListAt(list *list.List, index int) Any {
-	if index >= 0 && index < list.Len() {
-		for iter, i := list.Front(), 0; iter != nil; iter, i = iter.Next(), i+1 {
-			if i == index {
-				return iter.Value
-			}
-		}
+// 将列表元素反转，得到新列表
+func Reverse(l *list.List) *list.List {
+	rl := list.New()
+	for iter := l.Back(); iter != nil; iter = iter.Prev() {
+		rl.PushBack(iter.Value)
 	}
-	panic(fmt.Errorf("out of range [%d, %d]", 0, list.Len()))
+	return rl
 }
 
-func ListToSlice(list *list.List) []Any {
-	if list == nil {
-		panic(fmt.Errorf("invalid list"))
-	}
+// 定义结构体作为 map key
+type UserKey struct {
+	id   int
+	name string
+}
 
-	array := make([]Any, list.Len())
-	for iter, i := list.Front(), 0; iter != nil; iter, i = iter.Next(), i+1 {
-		array[i] = iter.Value
-	}
-	return array
+// 定义结构体作为 map value
+type UserValue struct {
+	gender   Char
+	birthday string
+	address  string
 }
