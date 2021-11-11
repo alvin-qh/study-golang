552606058f8ce2b075701662040d45d51ce18546
Alvin <quhao317@163.com>
2021-11-11 23:15:27 +0800

    [io] add path file demo

--- basic/io/file_path_test.go
+++ basic/io/file_path_test.go
@@ -1,1 +1,21 @@
 package io
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// 测试路径操作
+func TestPathOperation(t *testing.T) {
+	path, err := os.Getwd() // 获取当前路径
+	assert.NoError(t, err)
+	assert.True(t, strings.HasSuffix(path, "/basic/io"))
+
+	path, err = exec.LookPath("bash")
+    assert.NoError(t, err)
+	assert.NotEmpty(t, path)
+}
