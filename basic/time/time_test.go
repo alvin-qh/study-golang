6f6c7578f5c0c57959587c460db4866a28a46f2a
Alvin <quhao317@163.com>
2021-11-24 22:07:46 +0800

    [time] time demo

--- basic/time/time_test.go
+++ basic/time/time_test.go
@@ -8,20 +8,21 @@ import (
 )
 
 var (
-	TIME_LOCAL, _ = time.LoadLocation("Asia/Shanghai")
+	TIME_LOCAL, _ = time.LoadLocation("Asia/Shanghai") // 获取东八区时区
 
 	// Go 语言中格式化时间模板不是常见的 Y,M,S 等
-	// 而是 Go 语言的诞生时间 2006-01-02 15:04:05.000
-	TIME_LAYOUT_UTC = "2006-01-02T15:04:05.000Z"
+	// 而是 Go 语言的诞生时间 2006-01-02 15:04:05.000 MST
+	TIME_LAYOUT_UTC = "2006-01-02T15:04:05.000Z" // 以 Z 结尾的标准 UTC 时间格式
 	TIME_UTC        = "2021-11-11T12:00:00.100Z"
 
-	TIME_LAYOUT_OFF = "2006-01-02T15:04:05.000-07:00"
-	TIME_OFF        = "2021-11-11T12:00:00.100+08:00Z"
+	TIME_LAYOUT_OFF = "2006-01-02T15:04:05.000-07:00" // 带时区偏移量的时间格式
+	TIME_OFF        = "2021-11-11T12:00:00.100+08:00"
 
-	TIME_LAYOUT_ST = "2006-01-02 15:04:05 MST"
+	TIME_LAYOUT_ST = "2006-01-02 15:04:05 MST" // 带时间标准符合的时间格式
 )
 
 // 创建时间对象
+// 通过 time.Date 创建时间对象
 func TestCreateTime(t *testing.T) {
 	// 创建 UTC 时区时间对象
 	tm := time.Date(2012, 11, 11, 12, 0, 0, 0, time.UTC)
@@ -33,6 +34,7 @@ func TestCreateTime(t *testing.T) {
 }
 
 // 时区转换
+// 时间对象的 Location 函数获取该时间的时区对象；In 函数可以在不同时区转换时间；UTC 函数将时间转换为 UTC 时区
 func TestConvertLocalTime(t *testing.T) {
 	tm := time.Date(2012, 11, 11, 12, 0, 0, 0, time.UTC) // UTC 时区时间
 
@@ -50,117 +52,128 @@ func TestConvertLocalTime(t *testing.T) {
 }
 
 // 时间计算
+// 时间对象的 Sub 函数用于求两个时间对象的差，结果为 Duration 对象
+// 时间对象的 Add 函数用于求时间和一个 Duration 对象的结果
 func TestDuration(t *testing.T) {
 	tm1 := time.Date(2012, 11, 11, 12, 0, 0, 0, time.UTC)
 	tm2 := time.Date(2012, 11, 11, 20, 0, 0, 0, time.UTC)
 
-	d := tm2.Sub(tm1)
+	d := tm2.Sub(tm1) // 求两个时间的时间差
 	assert.Equal(t, float64(8), d.Hours())
 
-	tm3 := tm1.Add(d)
+	tm3 := tm1.Add(d) // 求一个时间经过一个时间差的结果
 	assert.Equal(t, tm2, tm3)
 
-	d = tm1.Sub(tm2)
+	d = tm1.Sub(tm2) // 时间差可以为负数
 	assert.Equal(t, float64(-8), d.Hours())
 
 	tm3 = tm2.Add(d)
 	assert.Equal(t, tm3, tm1)
 
-	d, err := time.ParseDuration("1h20m")
+	// 可以用一个字符串解析为时间差对象
+	d, err := time.ParseDuration("1h20m") // 时间差为 1 小时 20 分 0 秒
 	assert.NoError(t, err)
-	assert.Equal(t, int64(4800000000000), int64(d))
+	assert.Equal(t, int64(4800000000000), int64(d)) // 时间差转换为整数
 	assert.Equal(t, "1h20m0s", d.String())
 
-	d = d.Round(time.Hour)
-	assert.Equal(t, "1h0m0s", d.String())
+	// 时间差的 Round 函数获取与指定时间差的最近倍数
+	d, err = time.ParseDuration("1h20m") // 时间差为 1 小时 20 分 0 秒
+	assert.NoError(t, err)
 
-	d = d.Round(time.Minute * 35)
-	assert.Equal(t, "1h10m0s", d.String())
+	d = d.Round(time.Hour)                // 求时间差和 1 小时的倍数
+	assert.Equal(t, "1h0m0s", d.String()) // 结果为 1 小时 0 分 0 秒
 
-	mul := float64(d) / float64(time.Minute*35)
+	mul := float64(d) / float64(time.Hour) // 结果为 1 倍
+	assert.Equal(t, 1.0, mul)
+
+	d, err = time.ParseDuration("1h20m") // 时间差为 1 小时 20 分 0 秒
+	assert.NoError(t, err)
+
+	d = d.Round(time.Minute * 35)          // 求时间差和 35 分的倍数
+	assert.Equal(t, "1h10m0s", d.String()) // 结果为 1 小时 10 分 0 秒
+
+	mul = float64(d) / float64(time.Minute*35) // 结果为 2 倍
 	assert.Equal(t, 2.0, mul)
 }
 
+// 对时间进行 Round 和 Truncate 操作
+// Round 函数将时间计算为指定时间差的整数倍
+// Truncate 函数将时间计算为最接近的整点时间
 func TestRoundAndTruncate(t *testing.T) {
 	tm := time.Date(2012, 11, 11, 11, 40, 0, 0, time.UTC)
 	d := time.Hour * 12
 
-	tm = tm.Round(d)
+	tm = tm.Round(d) // 求指定时间为 12 小时整数倍的最近结果
 	assert.Equal(t, "2012-11-11 12:00:00 +0000 UTC", tm.String())
 
 	mul := float64(tm.UnixNano()) / float64(d)
-	assert.Equal(t, 31311.0, float64(mul))
+	assert.Equal(t, 31311.0, float64(mul)) // 倍数为 31311 倍
 
 	tm = time.Date(2012, 11, 11, 11, 40, 0, 0, time.UTC)
 	tm = tm.Truncate(d)
 	assert.Equal(t, "2012-11-11 00:00:00 +0000 UTC", tm.String())
 }
 
+// 时间格式化为字符串
 func TestTimeFormat(t *testing.T) {
 	tm := time.Date(2012, 11, 11, 12, 0, 0, 0, TIME_LOCAL)
 
 	s := tm.Format(TIME_LAYOUT_UTC)
-	assert.Equal(t, "2012-11-11T12:00:00.000Z", s)
+	assert.Equal(t, "2012-11-11T12:00:00.000Z", s) // 格式化为标准 UTC 格式
 
 	s = tm.Format(TIME_LAYOUT_OFF)
-	assert.Equal(t, "2012-11-11T12:00:00.000+08:00", s)
+	assert.Equal(t, "2012-11-11T12:00:00.000+08:00", s) // 格式化为带时区偏移量的格式
 
 	s = tm.Format(TIME_LAYOUT_ST)
-	assert.Equal(t, "2012-11-11 12:00:00 CST", s)
+	assert.Equal(t, "2012-11-11 12:00:00 CST", s) // 格式化为带标准时间标识的格式
 }
 
+// 将字符串转化为时间对象
 func TestParseTimeInUTC(t *testing.T) {
-	tm, err := time.Parse(TIME_LAYOUT_UTC, TIME_UTC)
-	assert.NoError(t, err)
-	assert.Equal(t, "2021-11-11 12:00:00.1 +0000 UTC", tm.String())
+	checkResult := func(tm *time.Time) {
+		assert.Equal(t, 2021, int(tm.Year()))
+		assert.Equal(t, 11, int(tm.Month()))
+		assert.Equal(t, 11, int(tm.Day()))
+		assert.Equal(t, 12, int(tm.Hour()))
+		assert.Equal(t, 0, int(tm.Minute()))
+		assert.Equal(t, 0, int(tm.Second()))
+		assert.Equal(t, 100000000, int(tm.Nanosecond()))
+	}
 
-	assert.Equal(t, 2021, int(tm.Year()))
-	assert.Equal(t, 11, int(tm.Month()))
-	assert.Equal(t, 11, int(tm.Day()))
-	assert.Equal(t, 12, int(tm.Hour()))
-	assert.Equal(t, 0, int(tm.Minute()))
-	assert.Equal(t, 0, int(tm.Second()))
-	assert.Equal(t, 100000000, int(tm.Nanosecond()))
+	tm, err := time.Parse(TIME_LAYOUT_UTC, TIME_UTC) // 将一个标准 UTC 格式的时间转换为时间对象
+	assert.NoError(t, err)
+	checkResult(&tm)
+
+	tm, err = time.Parse(TIME_LAYOUT_OFF, TIME_OFF) // 将一个带时区偏移量的字符串转为时间对象c
+	assert.NoError(t, err)
+	checkResult(&tm)
 }
 
-func TestParseTimeInCurrentLocale(t *testing.T) {
-	tm, err := time.Parse(TIME_LAYOUT_OFF, TIME_OFF)
-	assert.NoError(t, err)
-	assert.Equal(t, "2021-11-11 12:00:00.1 +0800 CST", tm.String())
-
-	assert.Equal(t, 2021, int(tm.Year()))
-	assert.Equal(t, 11, int(tm.Month()))
-	assert.Equal(t, 11, int(tm.Day()))
-	assert.Equal(t, 12, int(tm.Hour()))
-	assert.Equal(t, 0, int(tm.Minute()))
-	assert.Equal(t, 0, int(tm.Second()))
-	assert.Equal(t, 100000000, int(tm.Nanosecond()))
-}
-
+// 将时间进行序列化
 func TestTimeMarshal(t *testing.T) {
 	tm1 := time.Date(2012, 11, 11, 12, 0, 0, 0, time.UTC)
-	b, err := tm1.MarshalBinary()
+	b, err := tm1.MarshalBinary() // 序列化为 []byte
 	assert.NoError(t, err)
 
 	var tm2 time.Time
-	err = tm2.UnmarshalBinary(b)
+	err = tm2.UnmarshalBinary(b) // 从 []byte 反序列化
 	assert.NoError(t, err)
 
 	assert.Equal(t, tm1, tm2)
 
-	b, err = tm1.MarshalJSON()
+	b, err = tm1.MarshalJSON() // 序列化为 JSON 可用的格式
 	assert.NoError(t, err)
 	assert.Equal(t, "\"2012-11-11T12:00:00Z\"", string(b))
 
 	tm2 = time.Time{}
-	tm2.UnmarshalJSON([]byte("\"2012-11-11T12:00:00Z\""))
+	tm2.UnmarshalJSON([]byte("\"2012-11-11T12:00:00Z\"")) // 从 JSON 可用格式反序列化
 	assert.Equal(t, tm1, tm2)
 
-    b, err = tm1.MarshalText()
-    assert.NoError(t, err)
-    assert.Equal(t, "2012-11-11T12:00:00Z", string(b))
+	b, err = tm1.MarshalText() // 序列化为字符串
+	assert.NoError(t, err)
+	assert.Equal(t, "2012-11-11T12:00:00Z", string(b))
 
-    tm2 = time.Time{}
-	tm2.UnmarshalText([]byte("2012-11-11T12:00:00Z"))
+	tm2 = time.Time{}
+	tm2.UnmarshalText([]byte("2012-11-11T12:00:00Z")) // 从字符串反序列化
 	assert.Equal(t, tm1, tm2)
 }
--- basic/time/timer_test.go
+++ basic/time/timer_test.go
@@ -7,20 +7,78 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// 测试暂停当前线程（协程）
 func TestSleep(t *testing.T) {
 	tm := time.Now()
-	time.Sleep(3*time.Second + 2*time.Millisecond)
+	time.Sleep(3*time.Second + 2*time.Millisecond) // 休眠 3.2 秒
 
-	d := time.Since(tm)
+	d := time.Since(tm) // 计算当前时间和指定时间的差值，相当于 time.Now().Sub(tm)
 	assert.Equal(t, 3, int(d.Seconds()))
 }
 
+// 测试定时信号
+// 定时信号可以在指定时间后，通过 channel 发送一次性的定时信号
 func TestTimeAfter(t *testing.T) {
 	tm1 := time.Now()
 
-	c := time.After(3 * time.Second)
+	c := time.After(3 * time.Second) // 3 秒后发送信号
 
-	tm2 := <-c
-	d := tm2.Sub(tm1)
-    assert.Equal(t, 3, int(d.Seconds()))
+	tm2 := <-c // 等待信号到达
+
+	d := tm2.Sub(tm1) // 计算从发送信号到接收信号的时间差
+	assert.Equal(t, 3, int(d.Seconds()))
 }
+
+// 测试定时回调
+// 定时回调可以在指定时间后回调指定函数
+func TestTimeAfterFunc(t *testing.T) {
+	ch := make(chan struct{}) // 定义一个 channel
+	defer close(ch)
+
+	tm := time.Now()
+
+	time.AfterFunc(time.Second*3, func() { // 3 秒后回调函数
+		ch <- struct{}{} // 发送一个空的信号
+	})
+
+	<-ch // 等待信号到达
+
+	d := time.Since(tm) // 计算函数多久后进行回调
+	assert.Equal(t, 3, int(d.Seconds()))
+}
+
+// 测试周期性定时消息
+func TestTicker(t *testing.T) {
+	tm1 := time.Now()
+
+	tk := time.NewTicker(1 * time.Second) // 每隔 1 秒发送一次信号
+	defer tk.Stop()
+
+	n := 0
+	for n < 2 { // 获取两次信号
+		tm2 := <-tk.C // 等待信号到达
+
+		d := tm2.Sub(tm1) // 计算每次信号到达的时间
+		assert.Equal(t, n+1, int(d.Seconds()))
+
+		n++
+	}
+}
+
+// 测试一次性定时器
+func TestTimer(t *testing.T) {
+	tm1 := time.Now()
+
+	ti := time.NewTimer(2 * time.Second) // 2 秒后发送定时器信号
+	defer ti.Stop()
+
+	tm2 := <-ti.C // 等待定时器信号
+
+	d := tm2.Sub(tm1) // 计算信号到达时间
+	assert.Equal(t, 2, int(d.Seconds()))
+}
+
+// Timer 和 After 的异同
+//    Timer 和 After 均可以在指定时间后发送一个信号，以达到定时任务的效果
+//    Timer（或者 Ticker）具备更丰富的操作手段，包括 Stop（打断计时）和 Reset（重新计时)
+//    After 的功能比较简单
