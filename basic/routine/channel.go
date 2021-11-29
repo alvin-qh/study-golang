1a7ee746e61a450178798e8f31516acb5fcd14d0
Alvin <quhao317@163.com>
2021-11-29 22:19:45 +0800

    [goroutine] channel demo

--- basic/routine/channel.go
+++ basic/routine/channel.go
@@ -1,1 +1,67 @@
 package routine
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"sync/atomic"
+)
+
+var (
+	ErrLockClosed = errors.New("lock was closed")
+)
+
+type Lock struct {
+	ch     chan struct{}
+	mut    sync.Mutex
+	locked atomic.Value
+}
+
+func NewLock() *Lock {
+	l := &Lock{ch: make(chan struct{}, 1)}
+	l.locked.Store(false)
+	return l
+}
+
+func (l *Lock) Lock(ctx context.Context) bool {
+	for !l.locked.CompareAndSwap(false, true) {
+		ch := l.channel()
+
+		select {
+		case <-ch:
+			continue
+		case <-ctx.Done():
+			return false
+		}
+	}
+	return true
+}
+
+func (l *Lock) channel() <-chan struct{} {
+	l.mut.Lock()
+	defer l.mut.Unlock()
+
+	return l.ch
+}
+
+func (l *Lock) Unlock() {
+	if l.locked.CompareAndSwap(true, false) {
+		defer func() {
+			if recover() != nil {
+				l.ch = nil
+			}
+		}()
+
+		ch := l.swapChannel(make(chan struct{}, 1))
+		close(ch)
+	}
+}
+
+func (l *Lock) swapChannel(newOne chan struct{}) chan struct{} {
+	l.mut.Lock()
+	defer l.mut.Unlock()
+
+	oldOne := l.ch
+	l.ch = newOne
+	return oldOne
+}
--- basic/routine/channel_test.go
+++ basic/routine/channel_test.go
@@ -1,1 +1,39 @@
 package routine
+
+import (
+	"context"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	runtime.GOMAXPROCS(0)
+}
+
+func TestChanLock(t *testing.T) {
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+
+	ctx := context.TODO()
+
+	l := NewLock()
+
+	for i := 0; i < 3; i++ {
+		go func() {
+			defer wg.Done()
+
+			locked := l.Lock(ctx)
+			assert.True(t, locked)
+			if locked {
+				defer l.Unlock()
+				time.Sleep(time.Second)
+			}
+		}()
+	}
+
+	wg.Wait()
+}
