794e3d4d0dc619c0ee5204bd3171a4dd0c0cabac
Alvin <quhao317@163.com>
2025-01-18 02:23:34 +0800

    [embed] add static file embed demo

--- embed/static/static.go
+++ embed/static/static.go
@@ -7,12 +7,17 @@ import (
 
 // 通过 `go:embed` 指令将当前目录下的文件在编译时嵌入全局变量
 // `go:embed` 使用的约束包括: 1. 使用 `var` 修饰的变量; 2. 必须为全局变量
+// 一旦文件嵌入到全局变量中, 则其和普通全局变量无区别, 直接使用即可
+//
+// 文件可以嵌入为 `string` 类型或 `[]byte` 类型
 var (
+	// 嵌入为字符串类型
 	//go:embed static1.txt
 	STATIC_DATA string
 
+	// 嵌入为字节数组类型
 	//go:embed static2.json
-	STATIC_JSON string
+	STATIC_JSON []byte
 )
 
 // 用于解析 JSON 字符串的结构体
@@ -27,9 +32,8 @@ func GetEmbedStaticString() (*User, error) {
 	var u User
 
 	// 将嵌入字符串作为 JSON 格式进行反序列化
-	if err := json.Unmarshal([]byte(STATIC_JSON), &u); err != nil {
+	if err := json.Unmarshal(STATIC_JSON, &u); err != nil {
 		return nil, err
 	}
-
 	return &u, nil
 }
--- embed/static/static_test.go
+++ embed/static/static_test.go
@@ -6,13 +6,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// 测试通过全局变量
+// 测试将文件内容嵌入全局变量中
 func TestEmbed_GlobalStatic(t *testing.T) {
 	assert.Equal(t, "Hello World, This content was embed as static resource\n", STATIC_DATA)
 }
 
-// 
+// 测试将文件内容嵌入全局变量, 读取该变量并进行 json 反序列化
 func TestEmbed_LocalStatic(t *testing.T) {
+	// 读取反序列化结果
 	user, err := GetEmbedStaticString()
 
 	assert.Nil(t, err)
