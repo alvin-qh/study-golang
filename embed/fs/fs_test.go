9a5fa0eeac1de6d387deaca51390676e2f104841
AlvinQ <quhao317@163.com>
2025-01-23 21:38:01 +0800

    [embed] update demo

--- embed/fs/fs_test.go
+++ embed/fs/fs_test.go
@@ -6,35 +6,66 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// 测试列举 `embed.FS` 中嵌入的所有路径和文件
 func TestListFiles(t *testing.T) {
 	// 列举 `STATIC_ASSETS` 实例下的所有文件或路径信息
 	files, err := ListFiles(&STATIC_ASSETS)
 
 	assert.Nil(t, err)
 	assert.Equal(t, []FileItem{
-		{
-			Name: "asset",
-			Type: DIR,
-		},
-		{
-			Name: "asset/01",
-			Type: DIR,
-		},
-		{
-			Name: "asset/01/static1.txt",
-			Type: FILE,
-		},
-		{
-			Name: "asset/02",
-			Type: DIR,
-		},
-		{
-			Name: "asset/02/static2.txt",
-			Type: FILE,
-		},
-		{
-			Name: "asset/static.txt",
-			Type: FILE,
-		},
+		{Name: "asset", Type: DIR},
+		{Name: "asset/01", Type: DIR},
+		{Name: "asset/01/static1.txt", Type: FILE},
+		{Name: "asset/02", Type: DIR},
+		{Name: "asset/02/static2.txt", Type: FILE},
+		{Name: "asset/static.txt", Type: FILE},
 	}, files)
+}
+
+// 测试从 `embed.FS` 实例中读取指定文件的内容
+func TestReadEmbedFileFromFS(t *testing.T) {
+	fileNames := []string{
+		"asset/static.txt",
+		"asset/01/static1.txt",
+		"asset/02/static2.txt",
+	}
+
+	expectContent := []string{
+		"asset/static.txt file\n",
+		"asset/01/static1.txt file\n",
+		"asset/02/static2.txt file\n",
+	}
+
+	for n, name := range fileNames {
+		// 根据嵌入式文件的路径读取文件内容
+		data, err := STATIC_ASSETS.ReadFile(name)
+		assert.Nil(t, err)
+		assert.Equal(t, expectContent[n], string(data))
+	}
+}
+
+// 测试从 `embed.FS` 实例中读取指定文件的内容
+func TestOperateEmbedFileFromFS(t *testing.T) {
+	fileNames := []string{
+		"asset/static.txt",
+		"asset/01/static1.txt",
+		"asset/02/static2.txt",
+	}
+
+	expectContent := []string{
+		"asset/static.txt file\n",
+		"asset/01/static1.txt file\n",
+		"asset/02/static2.txt file\n",
+	}
+
+    fileOperate := func (index int, name string)  {
+        
+    }
+
+	for n, name := range fileNames {
+		// 根据嵌入式文件的路径读取文件内容
+		f, err := STATIC_ASSETS.Open(name)
+		assert.Nil(t, err)
+		assert.Equal(t, expectContent[n], string(data))
+	}
 }
