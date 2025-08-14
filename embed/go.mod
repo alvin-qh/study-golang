afd6441b6d23ffc0915e3bd6c99f48b36589c259
AlvinQ <quhao317@163.com>
2025-08-14 11:27:42 +0800

    [embed] update golang version

--- embed/go.mod
+++ embed/go.mod
@@ -1,4 +1,4 @@
-module embed-asset
+module study/embed
 
 go 1.25.0
 
--- embed/main.go
+++ embed/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "embed-asset/web"
+import "study/embed/web"
 
 func main() {
 	web.StartServer("0.0.0.0:12345")
